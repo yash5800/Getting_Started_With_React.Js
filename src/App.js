d7c79613cb329d0992cdaaac1efe4d5927826249
A.Yaswanth Kiran <yaswanthjk580@gmail.com>
2024-05-23 13:14:05 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Chat from './Chat';
+import logo from './logo.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [messages, setMessages] = useState([]);
+ 
+  const addMessage = (message)=>{
+    setMessages([...messages,message]);
+  };
+
+  return(
+        <Chat messages = {messages} addMessage = {addMessage}/>
   );
 }
 
---
+++ src/Chat.js
@@ -0,0 +1,54 @@
+import { useState, useRef } from 'react';
+import style from './index.module.css';
+
+function Chat({ messages, addMessage }) {
+    const [input, setInput] = useState('');
+    const textareaRef = useRef(null);
+
+    const handleChangeInput = (e) => {
+        setInput(e.target.value);
+        autoResizeTextarea(e.target);
+    };
+
+    const handleAdderInput = () => {
+        if (input.trim()) {
+            addMessage({ text: input, output: input });
+            setInput('');
+            textareaRef.current.style.height = 'auto';
+        }
+    };
+
+    const autoResizeTextarea = (textarea) => {
+        textarea.style.height = 'auto'; 
+        textarea.style.height = textarea.scrollHeight + 'px'; 
+    };
+
+    return (
+        <div className={style.chat_container}>
+            <div className={style.adder}>
+                {messages.map((msg, index) => (
+                    <div key={index}>
+                        <div className={style.msg}>
+                            <p>{msg.output}</p>
+                        </div>
+                        <div className={style.msg1}>
+                            <p>{msg.text}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className={style.input}>
+                <textarea
+                    ref={textareaRef}
+                    type='text'
+                    value={input}
+                    onChange={handleChangeInput}
+                    placeholder='Message Me'
+                />
+                <button type='submit' onClick={handleAdderInput}>Send</button>
+            </div>
+        </div>
+    );
+}
+
+export default Chat;
